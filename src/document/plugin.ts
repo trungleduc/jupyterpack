dd42f20bec35c1526d23b61293145e060d1c7a88
Duc Trung Le <leductrungxf@gmail.com>
2025-12-28 18:01:50 +0100

    add launcher items

--- src/document/plugin.ts
+++ src/document/plugin.ts
@@ -2,14 +2,15 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
-import { JupyterPackWidgetFactory } from './widgetFactory';
-import { IConnectionManager, IJupyterpackDocTracker } from '../type';
-import { IConnectionManagerToken, IJupyterpackDocTrackerToken } from '../token';
 import { WidgetTracker } from '@jupyterlab/apputils';
 import { DocumentWidget } from '@jupyterlab/docregistry';
-import { logoIcon } from '../tools';
+import { ILauncher } from '@jupyterlab/launcher';
+
+import { IConnectionManagerToken, IJupyterpackDocTrackerToken } from '../token';
+import { dashIcon, logoIcon, shinyIcon, streamlitIcon } from '../tools';
+import { IConnectionManager, IJupyterpackDocTracker } from '../type';
 import { addCommands } from './commands';
+import { JupyterPackWidgetFactory } from './widgetFactory';
 
 const FACTORY = 'jupyterpack';
 const CONTENT_TYPE = 'jupyterpack';
@@ -61,5 +62,71 @@ export const spkPlugin: JupyterFrontEndPlugin<IJupyterpackDocTracker> = {
     });
 
     return tracker;
+  }
+};
+
+export const launcherPlugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterpack:spklauncher',
+  requires: [ILauncher],
+  autoStart: true,
+  provides: IJupyterpackDocTrackerToken,
+  activate: (app: JupyterFrontEnd, launcher: ILauncher): void => {
+    const { commands } = app;
+    const commandId = 'jupyterpack:create-new-file';
+    const dashCommandId = 'jupyterpack:create-dash-app';
+    const streamlitCommandId = 'jupyterpack:create-streamlit-app';
+    const shinyCommandId = 'jupyterpack:create-shiny-app';
+    commands.addCommand(commandId, {
+      label: 'New SPK File',
+      icon: logoIcon,
+      caption: 'Create a new SPK fike',
+      execute: async () => {
+        console.log('AAAAAAAAAAAAAAA');
+      }
+    });
+    commands.addCommand(dashCommandId, {
+      label: 'Dash App',
+      icon: dashIcon,
+      caption: 'Create a new Dash Application',
+      execute: async () => {
+        console.log('AAAAAAAAAAAAAAA');
+      }
+    });
+    commands.addCommand(streamlitCommandId, {
+      label: 'Streamlit App',
+      icon: streamlitIcon,
+      caption: 'Create a new Streamlit Application',
+      execute: async () => {
+        console.log('AAAAAAAAAAAAAAA');
+      }
+    });
+    commands.addCommand(shinyCommandId, {
+      label: 'Shiny App',
+      icon: shinyIcon,
+      caption: 'Create a new Shiny Application',
+      execute: async () => {
+        console.log('AAAAAAAAAAAAAAA');
+      }
+    });
+    launcher.add({
+      command: commandId,
+      category: 'JupyterPack',
+      rank: 1
+    });
+    launcher.add({
+      command: dashCommandId,
+      category: 'JupyterPack',
+      rank: 2
+    });
+    launcher.add({
+      command: streamlitCommandId,
+      category: 'JupyterPack',
+      rank: 3
+    });
+    launcher.add({
+      command: shinyCommandId,
+      category: 'JupyterPack',
+      rank: 4
+    });
   }
 };
